c9844f39938a116a4fbbeb260580458b590871c6
nidhal baccouri <nidhalbacc@gmail.com>
2021-06-02 20:46:33 +0200

    changed main to cli

--- goli/__main__.py
+++ goli/__main__.py
@@ -3,7 +3,7 @@ from goli.example import hello
 
 
 @click.command(name="")
-def main(name: str) -> None:
+def cli(name: str) -> None:
 
     """Prints a greeting for a giving name example."""
     greeting: str = hello(name)
