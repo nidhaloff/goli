d5883cb34f483d4bbdcb695f5ea9951adee80c5c
nidhal baccouri <nidhalbacc@gmail.com>
2021-06-02 20:24:31 +0200

    updated license

--- goli/__main__.py
+++ goli/__main__.py
@@ -5,6 +5,6 @@ from goli.example import hello
 @click.command(name="")
 def main(name: str) -> None:
 
-    """Prints a greeting for a giving name."""
+    """Prints a greeting for a giving name example."""
     greeting: str = hello(name)
     click.echo(greeting)
