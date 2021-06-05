bc45f0e0f4c60805b4b2c43587aaea15c0d22881
nidhal baccouri <nidhalbacc@gmail.com>
2021-06-05 19:21:23 +0200

    reformated files

--- goli/cookiecutters.py
+++ goli/cookiecutters.py
@@ -1,38 +1,45 @@
+from typing import Mapping
+
+
 class CookieCutterTemplates:
-    MACHINE_LEARNING = "https://github.com/jeannefukumaru/cookiecutter-ml"
-    DATA_SCIENCE = "https://github.com/drivendata/cookiecutter-data-science"
-    DJANGO = "https://github.com/pydanny/cookiecutter-django"
-    FASTAPI = "https://github.com/arthurhenrique/cookiecutter-fastapi"
-    DJANGO_REST = "https://github.com/agconti/cookiecutter-django-rest"
-    FLASK = "https://github.com/cookiecutter-flask/cookiecutter-flask"
-    FLASK_REST = "https://github.com/karec/cookiecutter-flask-restful"
-    PY_PACKAGE = "https://github.com/audreyfeldroy/cookiecutter-pypackage"
-    PY_LIBRARY = "https://github.com/ionelmc/cookiecutter-pylibrary"
-    MODERN_PY_PACKAGE = "https://github.com/TezRomacH/python-package-template"
-    KOTLIN_ANDROID = (
+    MACHINE_LEARNING: str = "https://github.com/jeannefukumaru/cookiecutter-ml"
+    DATA_SCIENCE: str = (
+        "https://github.com/drivendata/cookiecutter-data-science"
+    )
+    DJANGO: str = "https://github.com/pydanny/cookiecutter-django"
+    FASTAPI: str = "https://github.com/arthurhenrique/cookiecutter-fastapi"
+    DJANGO_REST: str = "https://github.com/agconti/cookiecutter-django-rest"
+    FLASK: str = "https://github.com/cookiecutter-flask/cookiecutter-flask"
+    FLASK_REST: str = "https://github.com/karec/cookiecutter-flask-restful"
+    PY_PACKAGE: str = "https://github.com/audreyfeldroy/cookiecutter-pypackage"
+    PY_LIBRARY: str = "https://github.com/ionelmc/cookiecutter-pylibrary"
+    MODERN_PY_PACKAGE: str = (
+        "https://github.com/TezRomacH/python-package-template"
+    )
+    KOTLIN_ANDROID: str = (
         "https://github.com/general-mobile/kotlin-android-mvvm-starter"
     )
-    DOCKER_DATA_SCIENCE = (
+    DOCKER_DATA_SCIENCE: str = (
         "https://github.com/docker-science/cookiecutter-docker-science"
     )
-    PY_CLI = "https://github.com/nvie/cookiecutter-python-cli"
+    PY_CLI: str = "https://github.com/nvie/cookiecutter-python-cli"
 
     # golang:
-    GOLANG = "https://github.com/lacion/cookiecutter-golang"
+    GOLANG: str = "https://github.com/lacion/cookiecutter-golang"
 
-    FULL_STACK_FASTAPI_POSTGRES = (
+    FULL_STACK_FASTAPI_POSTGRES: str = (
         "https://github.com/tiangolo/full-stack-fastapi-postgresql"
     )
 
-    CPP = "https://github.com/hbristow/cookiecutter-cpp"
-    JAVA = "https://github.com/thomaslee/cookiecutter-java"
+    CPP: str = "https://github.com/hbristow/cookiecutter-cpp"
+    JAVA: str = "https://github.com/thomaslee/cookiecutter-java"
 
-    SWIFT = "https://github.com/theodesp/cookiecutter-swift-library"
-    SWIFT_IOS = "https://github.com/pmlbrito/cookiecutter-ios-template"
+    SWIFT: str = "https://github.com/theodesp/cookiecutter-swift-library"
+    SWIFT_IOS: str = "https://github.com/pmlbrito/cookiecutter-ios-template"
 
-    PHP = "https://github.com/jeromegamez/cookiecutter-php"
+    PHP: str = "https://github.com/jeromegamez/cookiecutter-php"
 
-    repos: object = {
+    repos: Mapping[str, Mapping[str, str]] = {
         "python": {
             "package": MODERN_PY_PACKAGE,
             "data-science": DATA_SCIENCE,
