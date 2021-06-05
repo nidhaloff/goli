325ba28d1a795719fb1c3126ff7ce985514c02f2
nidhal baccouri <nidhalbacc@gmail.com>
2021-06-05 19:11:59 +0200

    removed utils

--- goli/utils.py
+++
@@ -1,9 +0,0 @@
-from goli.constants import languages, topics
-
-
-def validate_args(*args) -> bool:
-    """ validate user input args """
-    for arg in args:
-        if arg not in topics or arg not in languages:
-            return False
-    return True
